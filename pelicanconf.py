00656cd30348e6f47b5caa4e88f084c7ea71c533
Ying Fang <fangying712@gmail.com>
2022-08-19 15:28:56 +0800

    Add submodule pelican-themes and pelican-plugins
    Signed-off-by: Ying Fang <fangying712@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
-AUTHOR = 'Yori Fang'
+AUTHOR = 'YoriFang'
 SITENAME = 'kernel.love'
-SITEURL = ''
-
-PATH = 'content'
-
+SITEURL = 'https://kernel.love'
 TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'zh'
+DATE_FORMATS = {'zh':'%Y-%m-%d %H:%M'}
+
+PATH = 'content'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -16,16 +16,67 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('kernel', 'https://kernel.org/'),
+        ('qemu.org', 'https://qemu.org/'),
+        ('zhihu', 'https://www.zhihu.com/people/fangying712'),
+        ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('https://github.com/fangying', '#'),
+        ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+# defaults
+DEFAULT_PAGINATION = False
+USE_FOLDER_AS_CATEGORY = True
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# markdown extensions
+md_extensions = [
+        "extra",
+        "toc",
+        "headerid",
+        "meta",
+        "sane_list",
+        "smarty",
+        "wikilinks",
+        "admonition",
+        "codehilite(pygments_style=emacs)"
+        ]
+
+# themes and plugins
+THEME = 'pelican-themes/elegant'
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = [
+        'assets',
+        'extract_toc',
+        'neighbors',
+        'render_math',
+        'related_posts',
+        'share_post',
+        'series'
+        ]
+
+MENUITEMS = (
+        ('Home', '/'),
+        ('Categories', '/categories.html'),
+        ('Tags', '/tags.html'),
+        ('Archives', '/archives.html'),
+        ('About', '/pages/about.html')
+        )
+
+SITEMAP = {
+        "format": "xml",
+        "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+        "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
+        }
+
+# do not generate draft docs
+IGNORE_FILES=['drafts/*']
+
+# SEO
+SITE_DESCRIPTION = (
+        "Happy Coding",
+        "Kernel & Love"
+        )
+
+# show site license
+SITE_LICENSE = u'<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"> kernelgo"</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://kernel.love" property="cc:attributionName" rel="cc:attributionURL">Yori Fang</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.'
