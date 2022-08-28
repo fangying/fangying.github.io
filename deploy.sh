9ca36d68e84351fdb9150ada616fba9ccee228dc
Ying Fang <fangying712@gmail.com>
2022-08-28 19:52:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 rm -rf .git
 cd output
-git init
+git init -b main
+echo "kernel.love" >> CNAME
 git add .
 git config user.name "Yori Fang"
 git config user.email "fangying2725@gmail.com"
 git config --global push.default simple
 git commit -m "latest release `date +"%Y-%m-%d %H:%M"`"
-git remote add upstream "git@github.com:YoriFang/yorifang.github.io.git"
-echo "kernel.love" >> CNAME
-git push -u upstream main --force
+git remote add origin "git@github.com:YoriFang/yorifang.github.io-src.git"
+git checkout -b html
+git push -u origin html:html --force
